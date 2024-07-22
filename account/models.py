9deb9d16f06487ec571d679f3e8d6f46ba7ee9cd
Tryd0g0lik <work80@mail.ru>
2024-07-22 14:38:11 +0700

    [master]: pause

--- account/models.py
+++ account/models.py
@@ -5,10 +5,10 @@ from .validators import min_length_validators
 # This's min int length of password
 passw_min_quantity_len: int = 10
 
+
 def update_min_length_validators(value: str):
   response = decorators_min_length_validators(min_length_validators)
   return response
-
 
 
 
