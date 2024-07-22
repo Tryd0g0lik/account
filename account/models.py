c6305347d01ec8d24e97fd753956c17aa658e832
Tryd0g0lik <work80@mail.ru>
2024-07-22 14:18:21 +0700

    [master]: pause

--- account/models.py
+++ account/models.py
@@ -2,10 +2,15 @@ from django.contrib.auth.models import User
 from account.dacorators import decorators_min_length_validators
 from .validators import min_length_validators
 
+# This's min int length of password
+passw_min_quantity_len: int = 10
 
 def update_min_length_validators(value: str):
   response = decorators_min_length_validators(min_length_validators)
   return response
+
+
+
 
 class UsersRegistrModel(User):
   '''
