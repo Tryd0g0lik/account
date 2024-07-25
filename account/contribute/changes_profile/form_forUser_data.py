72b240641f3484fae9e2680551c046252673a246
Tryd0g0lik <work80@mail.ru>
2024-07-25 11:53:59 +0700

    [2.2]: here code and tree was corrected/rename

---
+++ account/contribute/changes_profile/form_forUser_data.py
@@ -0,0 +1,40 @@
+from django import forms
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.contrib.messages.views import SuccessMessageMixin
+from django.shortcuts import get_object_or_404
+from django.urls import reverse_lazy
+from django.views.generic import UpdateView
+
+from account.models import UsersRegistrModel
+
+
+# Основных сведений пользователя
+class ChangeUserInfoForm(forms.ModelForm):
+  email = forms.EmailField(
+    required=True,
+    label="Адрес электронной почты"
+  )
+  
+  class Meta:
+    model = UsersRegistrModel
+    fields = ('username', 'email', 'first_name', 'last_name', 'send_messages')
+
+
+class ChangeInfoView(
+  SuccessMessageMixin, LoginRequiredMixin,
+  UpdateView):
+  model = UsersRegistrModel
+  template_name = 'users/change_user_info.html'
+  form_class = ChangeUserInfoForm
+  success_url = reverse_lazy('accounts:profile')
+  success_message = 'Данные пользователя изменены'
+  
+  def setup(self, request, *args, **kwargs):
+    self.user_id = request.user.pk
+    return super().setup(request, *args, **kwargs)
+  
+  def get_object(self, queryset=None):
+    if not queryset:
+      queryset = self.get_queryset()
+    return get_object_or_404(queryset, pk=self.user_id)
+    
--- account/views.py
+++ account/views.py
@@ -1,7 +1,7 @@
 from django.contrib.auth.views import LoginView, LogoutView
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth.mixins import LoginRequiredMixin
-from django.shortcuts import render, redirect
+from django.shortcuts import render
 
 from rest_framework.decorators import action
 from rest_framework.response import Response
@@ -10,8 +10,7 @@ from rest_framework.viewsets import ModelViewSet
 
 from .models import UsersRegistrModel
 from .serializers import Users_serializers
-from account.contribute.vews.template_authorizator import *
-from account.contribute.vews.template_registretor import *
+# Note!!:Others is by path 'account/contribute'
 
 class ALogoutView(LoginRequiredMixin, LogoutView):
     template_name = "users/logout.html"
@@ -37,58 +36,4 @@ class UsersAccountViews(ModelViewSet):
             return Response(status=status.HTTP_204_NO_CONTENT)
         except UsersAccountViews.DoesNotExist:
             return Response(status=status.HTTP_404_NOT_FOUND)
-
-# def form_authorisation_onPage(request):
-#     '''
-#     TODO: There page loading for user registration.
-#     :param request:
-#     :return:
-#     '''
-#     template_name_ = 'users/registration.html'
-#     if request.method != 'GET':
-#         return
-#
-#     authentication_form = CustomRegistrationForm()
-#     BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-#     file_static_css = \
-#         os.listdir(os.path.join(BASE_DIR, 'account\\static\\account\\css'))[-1]
-#     file_static_js = \
-#         os.listdir(os.path.join(BASE_DIR,
-#                                 'account\\static\\account\\javascripts'))[-1]
-#     context_ = {
-#         'form': authentication_form,
-#         'title': 'The User Account registration',
-#         'account_styles': file_static_css,
-#         'account_js': file_static_js
-#     }
-#     return render(request, template_name=template_name_, context=context_)
-
-# def register(request):
-#     if request.method == 'POST':
-#         form = UsersAccountViews(request.POST)
-#         if form.is_valid():
-#             form.save()
-#             username = form.cleaned_data.get('username')
-#             password = form.cleaned_data.get('password')
-#             user = authenticate(username=username,
-#                                 password=password)
-#             login(request, user)
-#             return redirect('home')
-#         else:
-#             form = UsersAccountViews()
-#             template_name_ = 'users/registration.html'
-#         return render(request,
-#                       template_name_,
-#                       {'form', form})
-
-
-# Create your views here.
-# class CustomAuthenticationView(LoginView):
-#   template_name = 'users/authorization.html'
-#   authentication_form = CustomRegistrationForm
-#   def from_valid(self, form):
-#     # Add your authentication logic
-#     return super().form_valid(form)
-
-
 
